119ac3cdf06ca35c27bab5bff569d118c61f2dc6
Robert Csakany <csakany.robert@gmail.com>
2024-09-23 11:52:16 +0200

    JNG-5932 Fixing to ignore gitignore in subdirectories

--- src/main/java/hu/blackbelt/judo/generator/commons/GitIgnoreSynchronizer.java
+++ src/main/java/hu/blackbelt/judo/generator/commons/GitIgnoreSynchronizer.java
@@ -116,7 +116,7 @@ public class GitIgnoreSynchronizer {
             // Appending marker and lines to be ignored
             newGitignoreLines.add(GENEARATOR_AREA_START);
             filesToIgnore.stream()
-                    .filter(entry -> !entry.getPath().equals(GIT_IGNORE))
+                    .filter(entry -> !entry.getPath().endsWith(GIT_IGNORE))
                     .forEach(entry -> {
                 newGitignoreLines.add(entry.getPath());
             });
